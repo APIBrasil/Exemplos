fa11262febc49ea8aa14d81a7d9e82e081c1c826
unknown <jhowjhoe@icloud.com>
2023-07-30 13:13:18 -0300

    new examples

---
+++ evolution/PHP/sendPool.php
@@ -0,0 +1,42 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+		CURLOPT_URL => 'https://cluster-01.apigratis.com/api/v1/evolution/message/sendPoll',
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'POST',
+		CURLOPT_POSTFIELDS =>'{
+            "number": "5531994359434",
+            "options": {
+              "delay": 1200,
+              "presence": "composing"
+            },
+            "pollMessage": {
+              "name": "Main text of the poll",
+              "selectableCount": 1,
+              "values": [
+                  "Question 1",
+                  "Question 2",
+                  "Question 3"
+              ]
+            }
+        }',
+		CURLOPT_HTTPHEADER => array(
+				'Content-Type: application/json',
+				'SecretKey: 703cdd9e-3ece-4d31-9066.....',
+				'PublicToken: 04adb4e2f055c978c9bb10.....',
+				'DeviceToken: 1c6f531d-c785-4655-bb70.....',
+				'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9......'
+		),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
