433b4ea9100d649fe6e47278aa9dbde8c996a630
unknown <jhowjhoe@icloud.com>
2023-07-30 12:08:01 -0300

    new examples

--- evolution/VB/sendText.vb
+++ evolution/VB/sendText.vb
@@ -5,9 +5,9 @@ Module MainModule
     Sub Main()
         Dim url As String = "https://cluster-01.apigratis.com/api/v1/evolution/message/sendText"
         Dim requestJson As String = "{""number"": ""5531994359434"",""options"": {""delay"": 1200,""presence"": ""composing""},""textMessage"": {""text"": ""Teste Evolution API, via APIBRASIL!""}}"
-        Dim secretKey As String = "703cdd9e-3ece-4d31-9066-c0b1cd2eadf7"
-        Dim publicToken As String = "04adb4e2f055c978c9bb101ee1bc5cd4"
-        Dim deviceToken As String = "1c6f531d-c785-4655-bb70-5e6adc5eb3de"
+        Dim secretKey As String = "703cdd9e-3ece-4d3....."
+        Dim publicToken As String = "04adb4e2f055c978c9....."
+        Dim deviceToken As String = "1c6f531d-c785-4655-bb70....."
         Dim bearerToken As String = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9...."
 
         Dim wc As New WebClient()
