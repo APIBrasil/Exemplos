34d20b06276f1c744ad306ec5ee28a33a08642bb
Jonathan Henrique <31408451+jhowbhz@users.noreply.github.com>
2023-09-12 14:55:18 -0300

    upgrade to v2

--- whatsapp/PHP/sendTextGet.php
+++ whatsapp/PHP/sendTextGet.php
@@ -13,7 +13,7 @@ if(!$number || !$text){
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-    CURLOPT_URL => 'https://cluster.apigratis.com/api/v1/whatsapp/sendText',
+    CURLOPT_URL => 'https://cluster.apigratis.com/api/v2/whatsapp/sendText',
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => '',
     CURLOPT_SSL_VERIFYHOST => false,
@@ -29,8 +29,6 @@ curl_setopt_array($curl, array(
   }',
     CURLOPT_HTTPHEADER => array(
         'Content-Type: application/json',
-        'SecretKey: OBTENHA_O_SEU_SECRET_TOKEN_NO_PAINEL',
-        'PublicToken: OBTENHA_O_SEU_PUBLIC_TOKEN_NO_PAINEL',
         'DeviceToken: OBTENHA_O_SEU_DEVICE_TOKEN_NO_PAINEL',
         'Authorization: Bearer OBTENHA_O_SEU_TOKEN'
     ),
